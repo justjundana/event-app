c41c31aa0efaf87507f6c759e94db1a4edbbe0eb
justjundana <jalbasyir@gmail.com>
2022-02-03 23:05:22 +0700

    Interface and Query for Get All Comments

--- repository/comment/comment.go
+++ repository/comment/comment.go
@@ -2,6 +2,9 @@ package comment
 
 import (
 	"database/sql"
+	"log"
+
+	_models "github.com/justjundana/event-planner/models"
 )
 
 type CommentRepository struct {
@@ -12,4 +15,27 @@ func New(db *sql.DB) *CommentRepository {
 	return &CommentRepository{
 		db: db,
 	}
+}
+
+func (r *CommentRepository) GetComments(eventID int) ([]_models.Comment, error) {
+	var comments []_models.Comment
+	rows, err := r.db.Query(`SELECT id, user_id, event_id, content FROM comments WHERE event_id = ?`, eventID)
+	if err != nil {
+		log.Fatalf("Error")
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var comment _models.Comment
+
+		err := rows.Scan(&comment.ID, &comment.UserID, &comment.EventID, &comment.Content)
+		if err != nil {
+			log.Fatalf("Error")
+		}
+
+		comments = append(comments, comment)
+	}
+
+	return comments, nil
 }
--- repository/comment/interface.go
+++ repository/comment/interface.go
@@ -1,4 +1,9 @@
 package comment
 
+import (
+	_models "github.com/justjundana/event-planner/models"
+)
+
 type CommentInterface interface {
+	GetComments(eventID int) ([]_models.Comment, error)
 }
