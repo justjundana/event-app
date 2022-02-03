7bef45303dfae959b6c4fc41a72a778539457d19
Sal-maa <salmaa.matematika@gmail.com>
2022-02-03 15:41:02 +0700

    add repo get event by location

--- repository/event/interface.go
+++ repository/event/interface.go
@@ -6,4 +6,5 @@ type EventInterface interface {
 	Get() ([]_models.Event, error)
 	GetById(id int) (_models.Event, error)
 	GetByKey(keyword string) (_models.Event, error)
+	GetByLocation(location string) (_models.Event, error)
 }
