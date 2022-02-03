e0ca11d94506a924d314beae7879ceda4549c382
justjundana <jalbasyir@gmail.com>
2022-02-03 23:37:05 +0700

    Interface and Query for Get All Participants

--- repository/participant/interface.go
+++ repository/participant/interface.go
@@ -1,4 +1,9 @@
 package participant
 
+import (
+	_models "github.com/justjundana/event-planner/models"
+)
+
 type ParticipantInterface interface {
+	GetParticipants(eventID int) ([]_models.Participant, error)
 }
--- repository/participant/participant.go
+++ repository/participant/participant.go
@@ -2,6 +2,9 @@ package participant
 
 import (
 	"database/sql"
+	"log"
+
+	_models "github.com/justjundana/event-planner/models"
 )
 
 type ParticipantRepository struct {
@@ -12,4 +15,27 @@ func New(db *sql.DB) *ParticipantRepository {
 	return &ParticipantRepository{
 		db: db,
 	}
+}
+
+func (r *ParticipantRepository) GetParticipants(eventID int) ([]_models.Participant, error) {
+	var participants []_models.Participant
+	rows, err := r.db.Query(`SELECT id, user_id, event_id, status FROM participants WHERE status = 1 AND event_id = ?`, eventID)
+	if err != nil {
+		log.Fatalf("Error")
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var participant _models.Participant
+
+		err := rows.Scan(&participant.ID, &participant.UserID, &participant.EventID, &participant.Status)
+		if err != nil {
+			log.Fatalf("Error")
+		}
+
+		participants = append(participants, participant)
+	}
+
+	return participants, nil
 }
