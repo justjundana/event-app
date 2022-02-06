b3fb1ef1b33d116807c728677822b747f0e88d0e
Sal-maa <salmaa.matematika@gmail.com>
2022-02-06 11:06:17 +0700

    edit repo get event by keyword to get event by search

--- repository/event/event.go
+++ repository/event/event.go
@@ -53,31 +53,18 @@ func (r *EventRepository) GetEvent(id int) (_models.Event, error) {
 	return event, nil
 }
 
-func (r *EventRepository) GetEventKeyword(keyword string) ([]_models.Event, error) {
+func (r *EventRepository) SearchEvents(keyword string) ([]_models.Event, error) {
 	var events []_models.Event
-	rows, err := r.db.Query(`SELECT id, user_id, image, title,category_id, description, location, date, quota FROM events WHERE title LIKE ? `, "%"+keyword+"%")
-	if err != nil {
-		log.Fatalf("Error")
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var event _models.Event
-
-		err := rows.Scan(&event.ID, &event.UserID, &event.Image, &event.Title, &event.CategoryId, &event.Description, &event.Location, &event.Date, &event.Quota)
-		if err != nil {
-			log.Fatalf("Error")
-		}
-
-		events = append(events, event)
-	}
-
-	return events, nil
-}
-
-func (r *EventRepository) GetEventLocation(location string) ([]_models.Event, error) {
-	var events []_models.Event
-	rows, err := r.db.Query(`SELECT id, user_id, image, title,category_id, description, location, date, quota FROM events WHERE location LIKE ?`, "%"+location+"%")
+	rows, err := r.db.Query(`
+	SELECT 
+		id, user_id, image, title,category_id, description, location, date, quota 
+	FROM
+		events 
+	WHERE 
+		CURRENT_TIMESTAMP < date AND (title LIKE ? OR location LIKE ?)
+	ORDER BY
+		date DESC
+		`, "%"+keyword+"%", "%"+keyword+"%")
 	if err != nil {
 		log.Fatalf("Error")
 	}
--- repository/event/interface.go
+++ repository/event/interface.go
@@ -5,8 +5,7 @@ import _models "github.com/justjundana/event-planner/models"
 type EventInterface interface {
 	GetEvents() ([]_models.Event, error)
 	GetEvent(id int) (_models.Event, error)
-	GetEventKeyword(keyword string) ([]_models.Event, error)
-	GetEventLocation(location string) ([]_models.Event, error)
+	SearchEvents(keyword string) ([]_models.Event, error)
 	GetEventMostAttendant() ([]_models.Event, error)
 	GetOwnEvent(userID int) ([]_models.Event, error)
 	CreateEvent(event _models.Event) error
